c2d2b53acfa105697a90fb7ca3f2fad12e79a5b0
Murad Aliyev <murada@maestro.st>
2024-12-24 22:23:01 +0400

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}","./public/**/*.html",
   ],
   theme: {
     extend: {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Əgər React istifadə edirsinizsə
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // İstəyirsinizsə bu dəyəri dəyişə bilərsiniz
+  },
+  server: {
+    port: 3000, // Yerli inkişaf serveri üçün port
+  },
+  preview: {
+    port: 5000, // İstehsal mühitində önizləmə üçün port
+  },
+  base: '/', // Domenə əsas URL kökü (domeninizin əsas yoludur)
+});
